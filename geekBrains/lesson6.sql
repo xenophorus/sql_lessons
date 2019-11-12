87f6677f31a6c3807b5f4762b3c8d0dd7c75191e
xenophorus <xenophorus@gmail.com>
2019-11-13 02:34:01 +1000

    cont

--- geekBrains/lesson6.sql
+++ geekBrains/lesson6.sql
@@ -19,8 +19,42 @@ where user_id = 1
     -- and media_type_id = (select id from media_types where name like 'par%')
 ;
 
-select * from media_types;
+select count(*) from media_types where name like '%xml%';
 
 
 select * from media
-where   user_id = 1;
+where user_id = 1;
+
+
+select count(*)
+from media
+where user_id = 1
+and media_type_id = 1
+;
+
+
+select
+    count(id),
+    monthname(created_at)
+from media
+group by month(created_at)
+order by month(created_at)
+;
+
+
+select
+(select name from users where id = user_id),
+count(user_id)
+from media
+group by user_id
+;
+
+-- HAVING
+
+describe fr_requests;
+
+select *
+from fr_requests
+where (initiator_user_id = 1 or targer_user_id = 1)
+    and status = 'approved'
+;
